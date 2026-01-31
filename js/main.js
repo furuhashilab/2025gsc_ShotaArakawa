e9c33f0c427dd37a078b5d2414cf7556c8c6b503
shocpsa <syota.arakawa0930@gmail.com>
2026-01-31 23:07:22 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -357,7 +357,7 @@ map.addControl(new maplibregl.NavigationControl(), "top-right");
   // ===== 行田市 境界線（GeoJSON） =====
   map.addSource("gyoda-boundary", {
     type: "geojson",
-    data: "./gyoda_geojson.geojson"   // ★拡張子つける
+    data: "./data/gyoda_geojson.geojson"   // ★拡張子つける
   });
 
   map.addLayer({
@@ -409,3 +409,4 @@ map.addControl(new maplibregl.NavigationControl(), "top-right");
 
 
  
+
