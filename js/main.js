213858c85dd13e438fbf8c0b96a6a0c2592bd8ec
shocpsa <syota.arakawa0930@gmail.com>
2026-02-05 22:47:57 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -376,7 +376,7 @@ map.addControl(new maplibregl.NavigationControl(), "top-right");
   // ===== 行田市 境界線（GeoJSON） =====
   map.addSource("gyoda-boundary", {
     type: "geojson",
-    data: "./data/gyoda_geojson.geojson"   // ★拡張子つける
+    data: "./gyoda_geojson.geojson"   // ★拡張子つける
   });
 
   map.addLayer({
